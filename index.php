0a65f4c7f09b3cff0f8d5e6f59076c8787843a54
abhi <programwithabhi@gmail.com>
2022-10-13 21:08:30 +0530

    First Commit

--- index.php
+++ index.php
@@ -1,16 +1,44 @@
 <?php
 
-    class AC{
-         public $speed;
-         public $model;
+    // class AC{
+    //      public $speed;
+    //      public $model;
 
-         function __construct($model,$speed){
-            echo $this->speed = $speed;
-            echo $this->model = $model;
-         }
+    //      function __construct($model,$speed){
+    //         echo $this->speed = $speed;
+    //         echo $this->model = $model;
+    //      }
 
-    }
+    // }
 
-    $abhi = new AC("Voltas","17");
+    // $abhi = new AC("Voltas","17");
+        class Abhi{
+            public $abhishek = 1;
+            public static $abhi = 1;
 
+            public function plus (){
+                echo $this->abhishek+1;
+                echo $this->abhishek;
+            }
+            public function minus (){
+                echo $this->abhishek-1;
+                // echo $this->abhishek;
+            }
+            public function  show(){
+                // echo $this->abhishek;
+                echo Abhi::$abhi;
+                echo "<br>";
+                echo Abhi::mainThing();
+            }
+            public static function mainThing(){
+                print_r('abhi class');
+
+            }
+        }
+        $abhi = new Abhi();
+        // $abhi->plus();
+        // $abhi->minus();
+        $abhi->show();
+        // Abhi::mainThing();
+        // echo Abhi::$abhi;
 ?>
---
+++ main.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Abhishek is the great programmer";
+    
+
+
+?>
