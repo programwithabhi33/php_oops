d8976ed4c1f50d40e3bd08dd2fe433a274d5fd54
abhi <programwithabhi@gmail.com>
2022-10-16 07:30:40 +0530

    Static in the php object orianted

---
+++ static.php
@@ -0,0 +1,22 @@
+<?php
+    class Abhi{
+        // static is the class variable that that it the common variable for all the objects 
+        public static $count = 0;
+        public $name;
+        public function main(){
+            echo Abhi::$count . "\n";
+            echo $this->name . "\n";
+        }
+        public function __construct($name){
+            $this->name = $name;
+            Abhi::$count++;
+        }
+    }
+    $abhi = new Abhi('Abhishek');
+    $abhi->main();
+    $abhi2 = new Abhi('Abhishek2');
+    $abhi2->main();
+
+
+
+?>
