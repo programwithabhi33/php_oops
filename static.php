a0861ea3a2d42d7a7f513bbd8c775536c6e00c78
abhi <programwithabhi@gmail.com>
2022-10-16 07:50:38 +0530

    static and non-static

--- static.php
+++ static.php
@@ -3,8 +3,12 @@
         // static is the class variable that that it the common variable for all the objects 
         public static $count = 0;
         public $name;
+        public static function main2(){
+            echo self::$count;
+        }
         public function main(){
-            echo self::$count . "\n";
+            // echo self::$count . "\n";
+            self::main2();
             echo $this->name . "\n";
         }
         public function __construct($name){
@@ -12,11 +16,28 @@
             self::$count++;
         }
     }
-    $abhi = new Abhi('Abhishek');
-    $abhi->main();
-    $abhi2 = new Abhi('Abhishek2');
-    $abhi2->main();
+    // $abhi = new Abhi('Abhishek');
+    // $abhi->main();
+    // $abhi2 = new Abhi('Abhishek2');
+    // $abhi2->main();
 
+    class sample extends Abhi{
+        // You cannot overide non-staic method from the base class methods to static in this class and static to non-static
+        public  function main(){
+            parent::main2();
+        }
+    }
+    $abhi = new sample('sample');
+    $abhi->main2();
+    
+    // class Abhi2{
+    //     public static $count = 0;
+    //     public function __construct(){
+    //         echo 'Hello World';
+    //     }
+    // }
+    // echo Abhi2::$count;
+    // $abhi = new Abhi2();
 
 
 ?>
