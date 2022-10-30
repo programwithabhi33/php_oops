0858cdc75bb65527af515f51dd51c592ac0cd3a2
abhi <programwithabhi@gmail.com>
2022-10-30 11:29:21 +0530

    Dependency Injection in the php object oriented

--- dependencyInjection.php
+++ dependencyInjection.php
@@ -14,13 +14,13 @@
         public function CreateUser(){
             $this->logger->log('User Created');
         }
-        public function __construct(Test $logger){
+        public function __construct(logger $logger){ //logger means you can only pass the logger class object otherwise this will give you error
             $this->logger = $logger;
         }
     }
     $log = new Test();
-    $abhi = new User($log);
+    $abhi = new User($log); //Passing the $log object to the User class,this is how User class dependet on the logger class and dependency injection works
     $abhi->CreateUser();
-
+ 
 
 ?>
