7de65c6d0876d8eab0362282734bef21c3bc8c21
abhi <programwithabhi@gmail.com>
2022-11-27 14:06:09 +0530

    method overriding in the using the traits

--- traits.php
+++ traits.php
@@ -5,9 +5,17 @@
         echo "Im the function of the train t1";
     }
  }
+ trait t2 {
+    function Test1(){
+        echo "Im the function of the train t2";
+    }
+ }
  class Abhi{
     // Here you can use the trait using the use statement and the trait name
-    use t1;
+    // use t1;
+
+    //You can use the multiple traits using the , 
+    use t1,t2;
  }
  $obj = new Abhi();
  $obj->Test1();
